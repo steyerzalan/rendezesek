4b9297875206ee76e8f0b1ca310378f6b8fcc87c
steyerzalan <steyerzalan@gmail.com>
2025-03-18 09:10:19 +0100

    kiegészültünk cserés rendezéssel

--- index.php
+++ index.php
@@ -25,3 +25,32 @@ for($i=$maxIndex;$i>=1;$i--)
 }
 
 print_r($tomb);
+echo "<br>";
+
+// cserés rendezés
+$tomb2=array(11,5,4,8,7,9);
+$maxIndex2=count($tomb2)-1;
+
+print_r($tomb2);
+echo "<br>";
+
+function csere2(&$tomb2, $i, $j)
+{
+    $b=$tomb2[$i];
+    $tomb2[$i]=$tomb2[$j];
+    $tomb2[$j]=$b;
+}
+
+for($i=0;$i<=$maxIndex2-1;$i++)
+{
+    for($j=$i+1;$j<=$maxIndex2;$j++)
+    {
+        if($tomb2[$i]>$tomb2[$j])
+        {
+            csere2($tomb2, $i, $j);
+        }
+    }
+}
+
+print_r($tomb2);
+echo "<br>";
