d627d3dfe0ac56fa59043b6e09f82066ca171cf1
steyerzalan <steyerzalan@gmail.com>
2025-03-18 08:23:38 +0100

    buborékos rendezés kísérlete

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+$tomb=array(11,5,4,8,7,9);
+print_r($tomb);
+echo "<br>";
+$maxIndex=count($tomb)-1;
+
+// & referencia, tömbben végzem el a cserét
+function csere(&$tomb, $j)
+{
+    $b=$tomb[$j+1];
+    $tomb[$j+1]=$tomb[$j];
+    $tomb[$j]=$b;
+}
+
+for($i=$maxIndex;$i>=1;$i--)
+{
+    for($j=0; $j<=$i-1;$j++)
+    {
+        if($tomb[$j]>$tomb[$j+1])
+        {
+            csere($tomb, $j);
+        }
+    }
+}
+
+print_r($tomb);
